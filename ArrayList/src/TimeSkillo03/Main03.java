3d358a7d806b78850d9e76eff2052fa2c54db63e
Ingrid Bini <binifidelis@gmail.com>
2025-09-18 10:51:43 -0300

    Menu time 03

--- ArrayList/src/TimeSkillo03/Main03.java
+++ ArrayList/src/TimeSkillo03/Main03.java
@@ -3,5 +3,15 @@ package TimeSkillo03;
 public class Main03 {
     public static void main(String[] args) {
 
+        System.out.println("""
+                ==== Menu ====
+                1 - Adicionar nome
+                2 - Listar nomes
+                3 - Remover nome
+                0 - Sair
+                """);
+
+
+
     }
 }
