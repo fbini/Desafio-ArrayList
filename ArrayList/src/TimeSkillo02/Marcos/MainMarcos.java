bd3d7ef1331d092fd6ab3ab95fb33efc4f60a0e5
Yuri <marcos.yuriro@gmail.com>
2025-09-18 18:42:21 -0300

    Protótipo Main

--- ArrayList/src/TimeSkillo02/Marcos/MainMarcos.java
+++ ArrayList/src/TimeSkillo02/Marcos/MainMarcos.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MainMarcos {
     public static void main(String[] args) {
-        System.out.println("a");
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> peopleList = new ArrayList();
+
+        int choice = -1;
+        while (choice != 0) {
+
+            System.out.println("""
+                    Menu De Escolhas:
+                    1 - Adicionar Nome
+                    2 - Listar Nomes
+                    3 - Remover Nome
+                    0 - Sair 
+                    """);
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Digite um nome:");
+                    String a = scanner.nextLine();
+                    String name = scanner.nextLine();
+                    peopleList.add(name);
+
+                }
+                case 2 -> {
+                    if (peopleList.size() == 0) {
+                        System.out.println("Lista Vazia.\n");
+                    } else {
+                        for (String person : peopleList) {
+                            System.out.println(person);
+                        }
+                    }
+                }
+
+                case 3 -> {
+
+                }
+
+                case 0 -> {
+                    System.out.println("Tchau!!");
+                }
+                default -> System.out.println("Opção Inválida!\n");
+            }
+        }
     }
 }
