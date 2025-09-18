430e3ea9f8adf07f53955dd0162a9d743aa9b246
Arthur <arthur1551@gmail.com>
2025-09-18 11:40:30 -0300

    File  main

--- ArrayList/src/TimeSkillo02/Main02.java
+++ ArrayList/src/TimeSkillo02/Main02.java
@@ -1,4 +1,14 @@
 package TimeSkillo02;
 
 public class Main02 {
+    public static void main(String[] args) {
+
+        System.out.println("""==== Menu ====
+                1 - Add name
+                2 - List name
+                3 - Remove name
+                0 - Exit 
+                """);
+
+    }
 }
