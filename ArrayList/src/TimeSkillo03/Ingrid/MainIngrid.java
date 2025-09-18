09259ad92440e2ddc5011f424ae406b6d5180fde
Ingrid Bini <binifidelis@gmail.com>
2025-09-18 11:47:48 -0300

    Entrega desafio arrays - Ingrid

--- ArrayList/src/TimeSkillo03/Ingrid/MainIngrid.java
+++ ArrayList/src/TimeSkillo03/Ingrid/MainIngrid.java
@@ -18,23 +18,55 @@ public class MainIngrid {
                     3 - Remover nome
                     0 - Sair
                     """);
+
+            System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
+            //limpa
             scanner.nextLine();
 
             switch (opcao) {
                 case 1:
-                    System.out.println("Digite um nome: ");
+                    System.out.print("Digite um nome: ");
                     String name = scanner.nextLine();
                     names.add(name);
-                    System.out.println("Nome adicionada");
+                    System.out.println("Nome adicionado!");
                     break;
 
                 case 2:
-                    System.out.println();
+                    // isEmpty retorna True se a lista tiver vazia
+                    if (names.isEmpty()) {
+                        System.out.println("A lista está vazia.");
+                    } else {
+                        System.out.println("Nomes na lista:");
+                        for (int i = 0; i < names.size(); i++) {
+                            System.out.println(i + " - " + names.get(i));
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Digite o índice para remover: ");
+                    int index = scanner.nextInt();
+                    scanner.nextLine(); // limpa
+                    if (index >= 0 && index < names.size()) {
+                        names.remove(index);
+                        System.out.println("Removido com sucesso!");
+                    } else {
+                        System.out.println("Índice inválido.");
+                    }
+                    break;
+
+                case 0:
+                    System.out.println("Saindo do programa");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                    break;
             }
 
         } while (opcao != 0);
 
-
+        scanner.close();
     }
 }
