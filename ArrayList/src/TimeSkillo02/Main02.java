a956bd8c3cd046e85c4da6e629b4bbc0ca089659
Kai <kailannyf.santos@gmail.com>
2025-09-20 09:27:55 -0300

    feat(TimeSkillo02): add base code to Main02.java

--- ArrayList/src/TimeSkillo02/Main02.java
+++ ArrayList/src/TimeSkillo02/Main02.java
@@ -1,23 +1,86 @@
-package TimeSkillo02;
+package src.TimeSkillo02;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main02 {
     public static void main(String[] args) {
-/*<<<<<<< HEAD
+        Scanner reading = new Scanner(System.in);
+        ArrayList<String> listOfNames = new ArrayList<>();
 
-        System.out.println("""==== Menu ====
-                1 - Add name
-                2 - List name
-                3 - Remove name
-                0 - Exit 
-                """);
+        int userOption;
 
-    }
-}
-=======
-        System.out.println("hello World");
-    }
-}
->>>>>>> da0ec26df30ee1c68260c702bc37d7c442c446fd*/
+        //Loop menu
+        do {
+            System.out.println("""
+                ==============  MENU  ===============
+        
+                         1 - Add name;
+                         2 - Edit name;
+                         3 - Remove name;
+                         4 - Show the name list;
+                         5 - Search something;
+                         0 - Quit.
+        
+                =====================================
+        """);
+            userOption = reading.nextInt();
+            reading.nextLine(); //Isso é para concertar o erro de leitura no próximo "reading.nextLine()"
 
+            switch(userOption) {
+                case 1:
+                    System.out.println("You chose to add a name");
+                    System.out.println("Which name do you  want to add to the list? ");
+                    String userAdd = reading.nextLine();
+                    listOfNames.add(userAdd);
+                    System.out.println("'" + userAdd + "' has been added to the list. ✅");
+                    break;
+                case 2:
+                    System.out.println("You chose to show the list");
+                    for (int i = 0; i < listOfNames.size(); i++) {
+                        System.out.println("Index " + i + ": " + listOfNames.get(i) + ";");
+                    }
+                    System.out.println("Which name do you want to edit by the index? ");
+                    int userEdit = reading.nextInt();
+                    System.out.println("Which name do you want to add? ");
+                    reading.nextLine(); //Isso é para concertar o erro de leitura no próximo "reading.nextLine()"
+                    String userName = reading.nextLine();
+                    listOfNames.set(userEdit, userName);
+                    System.out.println("'" + userName + "' the list has been edited. ✅");
+                    break;
+                case 3:
+                    System.out.println("You chose to remove a name");
+                    for (int i = 0; i < listOfNames.size(); i++) {
+                        System.out.println("Index " + i + ": " + listOfNames.get(i) + ";");
+                    }
+                    System.out.println("Which name do you want to remove by the index? ");
+                    int userRemove = reading.nextInt();
+                    listOfNames.remove(userRemove);
+                    System.out.println("'" + userRemove + "' has been removed from the list. ✅");
+                    break;
+                case 4:
+                    System.out.println("You chose to show the list");
+                    for (int i = 0; i < listOfNames.size(); i++) {
+                        System.out.println("Index " + i + ": " + listOfNames.get(i) + ";");}
+                    break;
+                case 5:
+                    System.out.println("You chose to search.");
+                    System.out.println("Searching...");
+                    String enterUser = reading.nextLine();
+                    if (listOfNames.contains(enterUser)) {
+                        System.out.println("The list contains " + enterUser);
+                    } else {
+                        System.out.println("We couldn't reach your search. ❌ Try again. \uD83D\uDD01");
+                        break;
+                    }
+                    break;
+                case 0:
+                    System.out.println("Okay! Quiting now! ✨");
+                    break;
+                default:
+                    System.out.println("Invalid entry. Pls try again! \uD83D\uDC40");
+                    break;
+            }
+        } while (userOption != 0);
     }
 }
